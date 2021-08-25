b2de5563266a54daca04b7cb48672b7171df0b7f
luisdavid <david.laleala@gmail.com>
2021-08-25 15:25:57 +0100

    changePORT

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = 3000;
+
 
 
 app.use(express.static('public'));
@@ -10,6 +10,4 @@ app.get('/', (req,res)=> {
    res.sendFile(path.join(__dirname, '', 'index.html'))
 })
 
-app.listen(port, function(){
-    console.log('Server is up at port ' + port )
-})
+app.listen(process.env.PORT || 5000)
