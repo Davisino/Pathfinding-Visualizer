ea8bf5a4c1d9006d7d51b2c965ee588b85bd345c
luisdavid <david.laleala@gmail.com>
2021-09-19 15:18:43 +0100

    Some bugs fixed

--- public/js/randomTarget.js
+++ public/js/randomTarget.js
@@ -2,18 +2,15 @@ import { rows } from "./app.js";
 import { cols } from "./app.js";
 // Handle edge case when randomTarget class is a wall;
 export function randomTargets() {
-    const randomStartBoxIdx = `${randomRow()}-${randomCol()}`;
-    const randomTargetBoxIdx = `${randomRow()}-${randomCol()}`;
+  const randomStartBoxIdx = `${randomRow()}-${randomCol()}`;
+  const randomTargetBoxIdx = `${randomRow()}-${randomCol()}`;
 
-    const startIdx = document.getElementById(randomStartBoxIdx);
-    const targetIdx = document.getElementById(randomTargetBoxIdx);
-    
-   return [randomStartBoxIdx, randomTargetBoxIdx]
+  return [randomStartBoxIdx, randomTargetBoxIdx];
 }
 
-function randomRow () {
-    return Math.floor(Math.random() * rows)
+function randomRow() {
+  return Math.floor(Math.random() * rows);
 }
-function randomCol () {
-    return Math.floor(Math.random() * cols);
+function randomCol() {
+  return Math.floor(Math.random() * cols);
 }
